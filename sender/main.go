e96399613224776a38e6d6254b6743786a2dc767
Joynal Abedin <joynal3483@gmail.com>
2019-05-15 07:36:13 +0600

    fixed sender

--- sender/main.go
+++ sender/main.go
@@ -12,12 +12,12 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 
-	webpush "github.com/SherClockHolmes/webpush-go"
+	"github.com/SherClockHolmes/webpush-go"
 )
 
 const dbUrl = "mongodb://localhost:27017"
 
-func handler(ctx context.Context, event events.KinesisEvent) (string, error) {
+func handler(ctx context.Context, event events.KinesisEvent) (bool, error) {
 	// Db connection stuff
 	dbCtx := context.Background()
 	dbCtx, cancel := context.WithCancel(dbCtx)
@@ -33,28 +33,41 @@ func handler(ctx context.Context, event events.KinesisEvent) (string, error) {
 
 	subscriberCol := db.Collection("notificationsubscribers")
 	var subscriberData core.SubscriberPayload
-		
 	for _, record := range event.Records {
-		_ = json.Unmarshal(record.Kinesis.Data, &subscriberData)
+		err = json.Unmarshal(record.Kinesis.Data, &subscriberData)
+
+		if err != nil {
+			fmt.Println("json err:", err)
+		}
 
 		// Decode subscription
 		s := &webpush.Subscription{}
 		_ = json.Unmarshal([]byte(subscriberData.PushEndpoint), s)
 
+		if err != nil {
+			fmt.Println("endpoint err:", err)
+		}
+
 		// Send Notification
-		_, err = webpush.SendNotification([]byte(subscriberData.Data), s, &subscriberData.Options)
+		res, err := webpush.SendNotification([]byte(subscriberData.Data), s, &subscriberData.Options)
+
+		fmt.Println("response:", res)
 
 		// TODO: find the correct code for unsubscribe
 		if err != nil {
 			fmt.Println("webpush error:", err)
-			_, _ = subscriberCol.UpdateOne(
+			_, err = subscriberCol.UpdateOne(
 				dbCtx,
 				bson.M{"_id": subscriberData.SubscriberID},
 				bson.M{"$set": bson.M{"status": "unSubscribed"}})
+
+			if err != nil {
+				fmt.Println("db update err:", err)
+			}
 		}
 	}
 
-	return "done", nil
+	return true, nil
 }
 
 func main() {
--- sender/main_test.go
+++ sender/main_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"github.com/SherClockHolmes/webpush-go"
+	"github.com/mongodb/mongo-go-driver/bson/primitive"
 	"lambda-push-go/core"
 
 	. "github.com/onsi/ginkgo"
@@ -12,17 +15,43 @@ var _ = Describe("parser lambda function", func() {
 	var (
 		ctx      context.Context
 		err      error
-		response string
+		response bool
 	)
 
 	Context("When the notification payload is plain text", func() {
 		BeforeEach(func() {
-			response, err = handler(ctx, core.ProcessEvent("Test string"))
+			notificationPayloadStr, _ := json.Marshal(core.NotificationPayload{
+				LaunchURL: "https://joynal.github.com",
+				Message: core.Message{
+					Title: "Fire on ice",
+					Message: "Bingo fire on ice returned",
+					Language: "en",
+				},
+			})
+
+			webPushOptions := webpush.Options{
+				Subscriber:      "https://joynal.github.com",
+				VAPIDPublicKey:  "BIUXk5gE6I8TtW5w7aoBgbA6i6o4MARqZ-fqNB7hhnzfP4hq6TIiXlqjqxn02hnuA1LBR_HmstAfTYVRBAUwAoA",
+				VAPIDPrivateKey: "pLQUD5C9TrkNXQ3DoppcTUkehY9YZ5KFbbIgBwISeZo",
+				TTL:             259200,
+			}
+
+			subscriberId, _ := primitive.ObjectIDFromHex("5cd664da6fc5221d583b0758")
+			endpoint := `{"endpoint":"https://updates.push.services.mozilla.com/wpush/v2/gAAAAABc2vqwO0MLYQKCoj05wzNv_hZZvJRxSuiAXVJUNYp3E1y95xMpmQpSPgNvwJdlm0G9Hn6ZOA0npgvCVfR9v15DNmzR4leRs7MutPRU-_ncR-LeyxVlt2GMIyMYyWpnhJSqkM1xJtzD7lW6KwekynkV_jeuo7UzEwpRi-RMrZqK1Abglcc","keys":{"auth":"jDbPxHsnmnFvjFnyGeZ11w","p256dh":"BHS8PiMlQ2D4SDi-HzFBdlu8e3gdZWA_DS0gBEO911K40NKi-BtT9wdXS5ZybtaJ4gwABvUwk2xXUQuud0aMwkU"}}`
+
+			data, _ := json.Marshal(core.SubscriberPayload{
+				PushEndpoint: endpoint,
+				Data:         string(notificationPayloadStr),
+				Options:      webPushOptions,
+				SubscriberID: subscriberId,
+			})
+
+			response, err = handler(ctx, core.ProcessEvent(string(data)))
 		})
 
-		It("Fails", func() {
-			Expect(err).To(MatchError("invalid character 'T' looking for beginning of value"))
-			Expect(response).To(Equal(""))
+		It("will send notification", func() {
+			Expect(err).To(BeNil())
+			Expect(response).To(Equal(true))
 		})
 	})
 })
