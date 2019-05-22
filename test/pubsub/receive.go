2d50732abdaa6f4171f661c7b8e5c1c7d6932103
Joynal Abedin <joynal3483@gmail.com>
2019-05-23 00:24:54 +0600

    pubsub test

--- test/pubsub/receive.go
+++ test/pubsub/receive.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"cloud.google.com/go/pubsub"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"sync"
+)
+
+func main() {
+	ctx := context.Background()
+
+	client, err := pubsub.NewClient(ctx, os.Getenv("GOOGLE_CLOUD_PROJECT"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var mu sync.Mutex
+	sub := client.Subscription("notification")
+	cctx, _ := context.WithCancel(ctx)
+	err = sub.Receive(cctx, func(ctx context.Context, msg *pubsub.Message) {
+		msg.Ack()
+		fmt.Printf("Got message: %q\n", string(msg.Data))
+		mu.Lock()
+		defer mu.Unlock()
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
