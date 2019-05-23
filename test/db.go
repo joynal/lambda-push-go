91c07aa593b4fed3d3d2d96027c8a493e9333370
Joynal Abedin <joynal3483@gmail.com>
2019-05-23 21:35:06 +0600

    db test added

---
+++ test/db.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/mongodb/mongo-go-driver/bson"
+	"lambda-push-go/core"
+	"log"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbUrl := os.Getenv("MONGODB_URL")
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	ctx = context.WithValue(ctx, core.DbURL, dbUrl)
+	db, err := core.ConfigDB(ctx, "omnikick")
+	if err != nil {
+		log.Fatalf("database configuration failed: %v", err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	var notification core.Notification
+	err = db.Collection("notifications").FindOne(ctx, bson.D{}).Decode(&notification)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("notification: %+v", notification)
+}
